4fd99f41719fff01adb0dd7c790e43032b18c215
ohzerov <ohzerov@proton.me>
2023-07-18 22:05:12 +0300

    calc for sabez

---
+++ calc.py
@@ -0,0 +1,9 @@
+def calculate():
+    expression = input("Введите математическое выражение: ")
+    try:
+        result = eval(expression)
+        print("Результат: ", result)
+    except Exception as e:
+        print("Ошибка: ", str(e))
+
+calculate()
